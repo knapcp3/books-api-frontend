2a2353e5952db877c50b3baa444e8d00d247b8fe
Jakub Knapik <jakubknapik3@gmail.com>
2018-12-27 16:43:26 +0100

    Add button and connect app to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+const baseUrl: string = "http://localhost:3005";
+
+interface ITitle {
+  title: string
+}
+
+class App extends Component<{}, ITitle> {
+  
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      title: ""
+    }
+  }
+
+  handleClick = () => {
+    fetch(`${baseUrl}/api/book/random-title`).then(res => {
+      return res.json();
+    }).then(res => {
+      console.log(res.title);
+      this.setState({
+        title: res.title
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <div>
         <header className="app-header">
           LELUM POLELUM
         </header>
+        <button onClick={this.handleClick}>random title from db!</button>
+        <p>{this.state.title}</p>
       </div>
     );
   }
